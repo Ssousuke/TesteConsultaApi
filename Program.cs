9aab50552deefd1aa46e62764cd1b4d4138d07e1
china <china@DESKTOP-G6M6RL0>
2023-01-09 20:27:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+class Program
+{
+    static async Task Main()
+    {
+        HttpClient httpClient = new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com") };
+        var response = await httpClient.GetAsync("posts");
+        var content = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+            Console.WriteLine("" + content);
+    }
+}
